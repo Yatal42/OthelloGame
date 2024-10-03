de33885caff04063ccece7f9511551531e9bbf79
Yatal42 <taltal2345@gmail.com>
2024-10-04 01:14:40 +0300

    First commit

---
+++ Ex02_Othelo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ex02_Othelo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OthelloGame.InitializeGame();
+            Console.ReadLine();
+        }
+    }
+}
